4cee9fa3ff0c71bbcf20d1a6522873e3d00fa7c9
Kaizer <hellsmenser@gmail.com>
2025-08-05 08:40:37 +0400

    404 handler

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ConfigProvider } from 'antd'
 import { customTheme } from './styles/theme'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './styles/global.css'
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root')
   if (root) {
     ReactDOM.createRoot(root).render(
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <HashRouter>
         <ConfigProvider theme={customTheme}>
           <App />
         </ConfigProvider>
-      </BrowserRouter>
+      </HashRouter >
     )
   } else {
     console.error('⚠️ #root not found')
